f0ce84d2901b3f5a0fca4888e419cc30036bbbf5
Nancy Du <nancyduyi@gmail.com>
2016-10-17 18:26:56 -0400

    Added additional file structure for server side. Server side now serving static client files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express();
 
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
 app.listen(8080);
 console.log('Listening on port 8080!')
 
