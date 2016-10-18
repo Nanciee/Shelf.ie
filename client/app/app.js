7dde763bfa52fe4f7edbeb15d80224af770e93d9
Nancy Du <nancyduyi@gmail.com>
2016-10-18 12:19:30 -0400

    Added account creation and authentication

--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,41 @@
-angular.module('store-front', ['store-front.manage', 'store-front.store', 'store-front.services', 'ngRoute'])
+angular.module('store-front', ['store-front.manage', 'store-front.store', 'store-front.services', 'store-front.auth', 'ngRoute'])
 .config(function ($routeProvider, $httpProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'app/manage/manage.html',
       controller: 'ManageController'
     })
+    .when('/signin', {
+      templateUrl: 'app/Auth/signin.html',
+      controller: 'AuthController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/Auth/signup.html',
+      controller: 'AuthController'
+    })
     .otherwise({
       redirectTo: '/'
     });
+
+  $httpProvider.interceptors.push('AttachTokens');
 })
+.factory('AttachTokens', function ($window) {
+  var attach = {
+    request: function (object) {
+      var jwt = $window.localStorage.getItem('com.shortly');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+.run(function ($rootScope, $location, Auth) {
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/signin');
+    }
+  });
+});
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,11 @@
 var infoController = require('../info/infoController.js')
+var userController = require('../user/userController.js')
 
 module.exports = function(app, express){
   app.get('/:url', infoController.checkInfo);
+
+  app.post('/api/users/signin', userController.signin);
+  app.post('/api/users/signup', userController.signup);
 
   app.get('/api/:url', infoController.getInfo)
   app.post('/api/user/info', infoController.addInfo)
